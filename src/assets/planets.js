d22ce33f71b339b1eec3adfcce41a33fd42b1fb7
Eric Guyader <ericguyader@yahoo.fr>
2018-05-29 18:04:23 +0200

    Affiche les planètes via des cards bootstrap.js

--- src/assets/planets.js
+++ src/assets/planets.js
@@ -1,7 +1,51 @@
 export default [
   {
     name: 'Terre',
+    description: 'La planète bleue, la seule habitée.',
     position: 3,
-    diametre: 12756
+    diametre: 12756,
+    matiere: 'rocheuse'
+  },
+  {
+    name: 'Mars',
+    description: 'La planète rouge, la prochaine à être colonisée.',
+    position: 4,
+    diametre: 6794,
+    matiere: 'rocheuse'
+  },
+  {
+    name: 'Mercure',
+    description: 'La planète la plus proche du soleil. Caliente!',
+    position: 1,
+    diametre: 4879,
+    matiere: 'rocheuse'
+  },
+  {
+    name: 'Venus',
+    description: 'La planète bleue, la seule habitée.',
+    position: 2,
+    diametre: 12104,
+    matiere: 'rocheuse'
+  },
+  {
+    name: 'Jupiter',
+    description: 'La planète célèbre pour ses anneaux.',
+    position: 5,
+    diametre: 139822,
+    matiere: 'gazeuse'
+  },
+  {
+    name: 'Uranus',
+    description: 'La planète bleue, la seule habitée.',
+    position: 7,
+    diametre: 50724,
+    matiere: 'gazeuse'
+  },
+  {
+    name: 'Saturne',
+    description: 'La planète bleue, la seule habitée.',
+    position: 6,
+    diametre: 116464,
+    matiere: 'gazeuse'
   }
 ]
--- src/components/PlanetList.vue
+++ src/components/PlanetList.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
+    <div class="row">
         <h3>
             Liste des planètes
         </h3>
-        <ul>
-            <li v-for="planete in planetes" :key="planete.position">
-                {{ planete.name }}
-            </li>
-        </ul>
+        <section class="row justify-content-md-center">
+            <div class="col-md-3" style="margin-top:1em" v-for="planete in planetes" :key="planete.position">
+                <div class="card">
+                    <img class="card-img-top" :src="'/assets/img/planets/' + planete.name + '.jpg'" :alt="planete.name">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ planete.name }}</h5>
+                        <p class="card-text">{{ planete.description }}</p>
+                        <a href="#" class="btn btn-primary">En savoir plus</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
