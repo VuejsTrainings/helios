0b3c13d3988b9495491adb9b94ce0203ff425b43
Eric Guyader <ericguyader@yahoo.fr>
2018-05-31 13:52:27 +0200

    Valide le formulaire d'édition des planètes

--- src/components/PlanetForm.vue
+++ src/components/PlanetForm.vue
@@ -1,22 +1,26 @@
 <template>
-  <form id="form">
+  <form id="form" ref="planetInfos" @submit.prevent="formSubmit">
     <h3>Informations sur la planète</h3>
     <ul>
       <li class="form-group">
         <label for="nomCtl">Nom</label>
-        <input type="text" class="form-control" id="nomCtl" placeholder="Terre">
+        <input type="text" class="form-control" id="nomCtl" placeholder="Terre"
+          v-model="planetObject.name" :required="required.indexOf('name')!==-1" >
       </li>
       <li class="form-group">
         <label for="positionCtl">Position</label>
-        <input type="number" class="form-control" min="1" step="1" id="positionCtl" placeholder="1">
+        <input type="number" class="form-control" min="1" step="1" id="positionCtl"
+          placeholder="1" v-model="planetObject.position" required>
       </li>
       <li class="form-group">
-        <label for="distanceCtl">Distance au soleil</label>
-        <input type="number" class="form-control" min="1" step="1" id="distanceCtl" placeholder="15620">
+        <label for="diametreCtl">Diamètre</label>
+        <input type="number" class="form-control" min="1" step="1" id="diametreCtl"
+          placeholder="15620" v-model="planetObject.diametre" required>
       </li>
       <li class="form-group">
         <label for="typeCtl">Type</label>
-        <select class="form-control" id="typeCtl">
+        <select class="form-control" id="typeCtl"
+            v-model="planetObject.matiere" required>
           <option>rocheuse</option>
           <option>gazeuse</option>
           <option>naine</option>
@@ -24,26 +28,64 @@
       </li>
       <li class="form-group">
         <label for="descriptionCtl">Description</label>
-        <textarea class="form-control" id="descriptionCtl" rows="5"></textarea>
+        <textarea class="form-control" id="descriptionCtl" rows="5"
+            v-model="planetObject.description" required></textarea>
       </li>
       <li class="form-group">
         <label for="htmlCtl">Article</label>
-        <textarea class="form-control" id="htmlCtl" rows="5"></textarea>
+        <textarea class="form-control" id="htmlCtl" rows="5" v-model="planetObject.html"></textarea>
       </li>
-    <button type="submit" class="btn btn-success">Submit</button>
+    <button type="submit" class="btn btn-success" :disabled="notValid">Submit</button>
     </ul>
   </form>
 </template>
 
 <script>
+import Planetes from '@/assets/planets.js'
 export default {
   name: 'PlanetForm',
-  props: ['planete']
+  props: ['planete'],
+  data () {
+    let planetes = Planetes
+    let planet = planetes.find(chqPlanete => chqPlanete.name === this.planete)
+    return {
+      planetObject: planet,
+      required: ['name', 'position', 'diametre', 'matiere']
+    }
+  },
+  computed: {
+    notValid () {
+      let valid = true
+      this.required.forEach(att => { valid = valid && !(this.planetObject[att] === '') })
+      return !valid
+    }
+  },
+  methods: {
+    formSubmit (ev) {
+      // Validation du formulaire
+
+      // Transmission des données pour leur persistance
+
+      // Envoi information + "rediriger"
+
+    }
+  }
 }
 </script>
 
 <style scoped>
 li.form-group{
   list-style-type: none;
+}
+:invalid{
+  border-color:#f00;
+}
+:invalid:focus{
+  background-color:#fdd;
+}
+.form-control:valid:focus{
+  border-color: #090;
+  background:#dfd;
+  box-shadow:0 0 0 0.2rem #9f9;
 }
 </style>
