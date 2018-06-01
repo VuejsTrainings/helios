53e6be4e855d69bc77f6393270420c60fadf8f88
Eric Guyader <ericguyader@yahoo.fr>
2018-06-01 10:59:48 +0200

    AJoute et configure vue-resource

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 import Planetes from '@/assets/planets.js'
 
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
